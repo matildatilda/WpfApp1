2435c7e326c97d2682de0e5ceab83aa350e21079
matildatilda <melty.bombom@gmail.com>
2025-09-22 13:59:13 +0900

    テストケース追加

--- TestProject1/ImageManagerViewModelTest.cs
+++ TestProject1/ImageManagerViewModelTest.cs
@@ -10,5 +10,13 @@
             vm.GetImageInfoModelList();
             Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void TestMethod_GetImageInfoModelListInternal()
+        {
+            var vm = new WpfApp1.ImageManagerViewModel();
+            var result = vm.GetImageInfoModelListInternal();
+            Assert.AreEqual(3, result.ToList().Count);
+        }
     }
 }
