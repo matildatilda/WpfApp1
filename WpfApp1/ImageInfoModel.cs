91a9f3fda28f242918da7402bae4a35faf582874
matildatilda <melty.bombom@gmail.com>
2025-09-22 13:01:18 +0900

    コード分析結果反映

--- WpfApp1/ImageInfoModel.cs
+++ WpfApp1/ImageInfoModel.cs
@@ -5,45 +5,47 @@ using System.Runtime.InteropServices;
 // モデルはデータの保持だけをするのでメンバーとgetter/setterだけでメソッドはありません。
 // データを入れておく形の決まった箱のようなものです。
 // このサンプルではモデルは１つですが実際は複数のモデルがあるはず。
-
-public class ImageInfoModel
+namespace WpfApp1
 {
-
-    private string imageName;   // 画像名
-    public string ImageName { get { return imageName; } set { imageName = value; } }
-
-    private string imageUrl;    // 画像ファイルURL
-    public string ImageUrl { get { return imageUrl;  } set { imageUrl = value; } }
-
-    private string createdBy;   // 作成者
-    public string CreatedBy { get { return createdBy; } set { createdBy = value; } }
-
-    private string createdAt;    // 作成日時
-    public string CreatedAt { get { return createdAt; } set { createdAt = value; } }
-
-    private int confidentialMode;  // 機密 1:公開, 2: 社内外関係者のみ, 3: 社外秘
-    public int ConfidentialMode { get { return confidentialMode; } set  { confidentialMode = value; } }
-
-    private string description;     // 説明
-    public string Description { get { return description; } set { description = value; } }
-
-    public ImageInfoModel()
-	{
-        this.imageName = "";
-        this.imageUrl = "";
-        this.createdBy = "";
-        this.createdAt = "";
-        this.confidentialMode = 1;
-        this.description = "";
-	}
-
-    public ImageInfoModel(string imageName, string imageUrl, string createdBy, string createdAt, int confidentialMode, string description)
+    public class ImageInfoModel
     {
-        this.imageName = imageName;
-        this.imageUrl = imageUrl;
-        this.createdBy = createdBy;
-        this.createdAt = createdAt;
-        this.confidentialMode = confidentialMode;
-        this.description = description;
+
+        private string imageName;   // 画像名
+        public string ImageName { get { return imageName; } set { imageName = value; } }
+
+        private string imageUrl;    // 画像ファイルURL
+        public string ImageUrl { get { return imageUrl; } set { imageUrl = value; } }
+
+        private string createdBy;   // 作成者
+        public string CreatedBy { get { return createdBy; } set { createdBy = value; } }
+
+        private string createdAt;    // 作成日時
+        public string CreatedAt { get { return createdAt; } set { createdAt = value; } }
+
+        private int confidentialMode;  // 機密 1:公開, 2: 社内外関係者のみ, 3: 社外秘
+        public int ConfidentialMode { get { return confidentialMode; } set { confidentialMode = value; } }
+
+        private string description;     // 説明
+        public string Description { get { return description; } set { description = value; } }
+
+        public ImageInfoModel()
+        {
+            this.imageName = "";
+            this.imageUrl = "";
+            this.createdBy = "";
+            this.createdAt = "";
+            this.confidentialMode = 1;
+            this.description = "";
+        }
+
+        public ImageInfoModel(string imageName, string imageUrl, string createdBy, string createdAt, int confidentialMode, string description)
+        {
+            this.imageName = imageName;
+            this.imageUrl = imageUrl;
+            this.createdBy = createdBy;
+            this.createdAt = createdAt;
+            this.confidentialMode = confidentialMode;
+            this.description = description;
+        }
     }
 }
--- WpfApp1/ImageManagerViewModel.cs
+++ WpfApp1/ImageManagerViewModel.cs
@@ -23,96 +23,99 @@ using WpfApp1;
 // このファイル内で渡すときは、戻り値と引数が一致している関数を、あたかも変数のように渡しています。
 // this.GetImageInfoListCommand = new GetImageInfoListCommand(this.GetImageInfoModelList);
 
-public class ImageManagerViewModel : INotifyPropertyChanged
+namespace WpfApp1
 {
-	// モデルの実態（インスタンス）
-	private List<ImageInfoModel> imageInfoModels;
-	public List<ImageInfoModel> ImageInfoModels
-	{  
-		get { return imageInfoModels; }
-		set { imageInfoModels = value; OnPropertyChanged(nameof(ImageInfoModels)); }
-	}
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-	protected virtual void OnPropertyChanged(string propertyName)
+	public class ImageManagerViewModel : INotifyPropertyChanged
 	{
-		Debug.WriteLine($"OnPropertyChanged: {propertyName}");
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-	}
-
-    // (1) 画像情報取得コマンド：画像情報取得ボタンのクリックイベントで呼ばれる
-    public ICommand GetImageInfoListCommand { get; }
-
-	private readonly Dispatcher _dispatcher;
-	private bool _isGettingImage = false;
-	private readonly object _lockObject = new object();
-
-	public ImageManagerViewModel()
-	{
-		// モデルを初期化
-		imageInfoModels = new List<ImageInfoModel>();
-		// 画像情報取得コマンドをインスタンス化、この時画像情報取得処理を渡す
-		this.GetImageInfoListCommand = new GetImageInfoListCommand(this.GetImageInfoModelList, this.CanGetImage);
-		_dispatcher = Dispatcher.CurrentDispatcher;
-	}
-    
-	private bool CanGetImage()
-	{
-		return !_isGettingImage;
-	}
-
-	public void GetImageInfoModelList()
-	{
-		lock (_lockObject) 
+		// モデルの実態（インスタンス）
+		private List<ImageInfoModel> imageInfoModels;
+		public List<ImageInfoModel> ImageInfoModels
 		{
-			if (_isGettingImage) return;
-			_isGettingImage = true;
+			get { return imageInfoModels; }
+			set { imageInfoModels = value; OnPropertyChanged(nameof(ImageInfoModels)); }
 		}
 
-		try
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected virtual void OnPropertyChanged(string propertyName)
 		{
-			if (_dispatcher.CheckAccess())
-			{
-				GetImageInfoModelListInternal();
-			}
-			else
-			{
-				_dispatcher.Invoke(GetImageInfoModelListInternal);
-			}
+			Debug.WriteLine($"OnPropertyChanged: {propertyName}");
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
-		finally
+
+		// (1) 画像情報取得コマンド：画像情報取得ボタンのクリックイベントで呼ばれる
+		public ICommand GetImageInfoListCommand { get; }
+
+		private readonly Dispatcher _dispatcher;
+		private bool _isGettingImage = false;
+		private readonly object _lockObject = new();
+
+		public ImageManagerViewModel()
+		{
+			// モデルを初期化
+			imageInfoModels = [];
+			// 画像情報取得コマンドをインスタンス化、この時画像情報取得処理を渡す
+			this.GetImageInfoListCommand = new GetImageInfoListCommand(this.GetImageInfoModelList, this.CanGetImage);
+			_dispatcher = Dispatcher.CurrentDispatcher;
+		}
+
+		private bool CanGetImage()
+		{
+			return !_isGettingImage;
+		}
+
+		public void GetImageInfoModelList()
 		{
 			lock (_lockObject)
 			{
-				_isGettingImage = false;
+				if (_isGettingImage) return;
+				_isGettingImage = true;
 			}
 
-			CommandManager.InvalidateRequerySuggested();
+			try
+			{
+				if (_dispatcher.CheckAccess())
+				{
+					GetImageInfoModelListInternal();
+				}
+				else
+				{
+					_dispatcher.Invoke(GetImageInfoModelListInternal);
+				}
+			}
+			finally
+			{
+				lock (_lockObject)
+				{
+					_isGettingImage = false;
+				}
+
+				CommandManager.InvalidateRequerySuggested();
+			}
+
 		}
 
+		// (3) 画像情報取得処理の実装
+		public List<ImageInfoModel> GetImageInfoModelListInternal()
+		{
+			Debug.WriteLine("start GetImageInfoModelList");
+
+			List<ImageInfoModel> imageInfoModelList = [];
+
+			ImageInfoModel imageInfoModel1 = new("image_1", "https://image-server-1/image_1.png", "Ichiro Yamada", "2025/9/1 14:00:00", 1, "");
+			imageInfoModelList.Add(imageInfoModel1);
+
+
+			ImageInfoModel imageInfoModel2 = new("image_2", "https://image-server-1/image_2.jpg", "Jiro Sato", "2025/9/3 16:00:00", 2, "");
+			imageInfoModelList.Add(imageInfoModel2);
+
+			ImageInfoModel imageInfoModel3 = new("image_3", "https://image-server-1/image_3.png", "Ichiro Yamada", "2025/9/1 15:00:00", 3, "");
+			imageInfoModelList.Add(imageInfoModel3);
+
+			ImageInfoModels = imageInfoModelList;
+
+			Debug.WriteLine("end GetImageInfoModelList");
+
+			return imageInfoModelList;
+		}
 	}
-
-    // (3) 画像情報取得処理の実装
-    public List<ImageInfoModel> GetImageInfoModelListInternal()
-	{
-		Debug.WriteLine("start GetImageInfoModelList");
-
-		List<ImageInfoModel> imageInfoModelList = new List<ImageInfoModel>();
-
-		ImageInfoModel imageInfoModel1 = new ImageInfoModel("image_1", "https://image-server-1/image_1.png", "Ichiro Yamada", "2025/9/1 14:00:00", 1, "");
-		imageInfoModelList.Add(imageInfoModel1);
-
-
-        ImageInfoModel imageInfoModel2 = new ImageInfoModel("image_2", "https://image-server-1/image_2.jpg", "Jiro Sato", "2025/9/3 16:00:00", 2, "");
-		imageInfoModelList.Add(imageInfoModel2);
-
-        ImageInfoModel imageInfoModel3 = new ImageInfoModel("image_3", "https://image-server-1/image_3.png", "Ichiro Yamada", "2025/9/1 15:00:00", 3, "");
-		imageInfoModelList.Add(imageInfoModel3);
-
-		ImageInfoModels = imageInfoModelList;
-
-        Debug.WriteLine("end GetImageInfoModelList");
-        
-		return imageInfoModelList;
-    }
 }
